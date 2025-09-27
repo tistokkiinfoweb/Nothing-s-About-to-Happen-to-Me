5fa081fa6a9cf04686ed94da636ff8560e5d1fba
Thaís/Tis <thais.g@escolar.ifrn.edu.br>
2025-09-27 20:28:03 -0300

    Update tela principal.py

--- tela principal.py
+++ tela principal.py
@@ -4,25 +4,25 @@ from sys import exit
 
 pygame.init()
 
-musica_de_fundo = pygame.mixer.music.load()
+musica = pygame.mixer_music.load('AC_DC-HellsBells.mp3')
 pygame.mixer.music.play(-1)
 
-largura = 640
-altura = 480
-
-fonte = pygame.font.SysFont('Inter', 50, False)
-tela = pygame.display.set_mode((largura, altura))
+tela = pygame.display.set_mode([920,720])
 pygame.display.set_caption('Adventure Cat')
 
-while True:
-    tela.fill((255,255,255))
+fundo = pygame.image.load('')
+tela.blit(fundo, (0,0))
+
+fonte = pygame.font.SysFont('mago3', 100, False)
+rodando = True
+
+while rodando == True:
     mensagem = f'Adventure Cat'
-    texto_formatado = fonte.render(mensagem,True,(0,0,0))
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
+    texto = fonte.render(mensagem, True, (255,255,255))
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            loop = False
             exit()
-
-    tela.blit(texto_formatado, (180,160))
-
+    
+    tela.blit(texto, (180,160))
     pygame.display.flip()
